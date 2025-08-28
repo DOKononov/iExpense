50b0e224c5c74177fbc335caea86f637c48870d3
d.o.kononov <d.o.kononov@gmail.com>
2025-08-28 16:10:04 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dmitry Kononov on 28.08.25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
