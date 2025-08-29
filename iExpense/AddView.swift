4edd90bd0638a858d5271bde0f0ab110393902d0
d.o.kononov <d.o.kononov@gmail.com>
2025-08-29 17:35:29 +0300

    finish

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    var expendses: Expenses
+    let types = ["Buisness", "Personal"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+                
+            }
+            .navigationTitle("Add new expense")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button("Save") {
+                    let newItem = ExpenseItem(name: name, type: type, amount: amount)
+                    expendses.items.append(newItem)
+                    dismiss()
+                }
+            }
+        }
+
     }
 }
 
 #Preview {
-    AddView()
+    AddView(expendses: Expenses())
 }
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,25 +7,66 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
 }
 
-
-struct ContentView: View {
-    @State private var user = User(firstName: "Tailor", lastName: "Swift")
-    var body: some View {
-        VStack {
-            Button("Save User") {
-                let encoder = JSONEncoder()
-                if let data = try? encoder.encode(user) {
-                    UserDefaults.standard.set(data, forKey: "UserData")
-                }
+@Observable
+class Expenses {
+    var items: [ExpenseItem] = [] {
+        didSet {
+            if let data = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(data, forKey: "Items")
             }
         }
     }
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: "Items") {
+            let savedItems = try? JSONDecoder().decode([ExpenseItem].self, from: data)
+            items = savedItems ?? []
+        }
+    }
+}
 
+struct ContentView: View {
+    @State private var expenses: Expenses = .init()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expendses: expenses)
+            }
+        }
+    }
+    
+    private func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
 }
 
 #Preview {
